be1059a4fd13bdfc3bfb68a9959a50200ffe74d6
worldmozara <worldmozara@163.com>
2022-11-02 12:35:25 +0800

    add version file.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tkreform',
-    version='0.1.0',
+    version=open("VERSION").read(),
     description='Reformed tkinter coding tool.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
